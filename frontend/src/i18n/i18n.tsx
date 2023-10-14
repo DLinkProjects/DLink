34b383c3a8b4a77bb8d265882fdb5b063afc4b01
qianmianyao <zp1998421@gmail.com>
2023-10-14 22:50:51 +0800

    🐞 fix(frontend): 修复英文显示错误

--- frontend/src/i18n/i18n.tsx
+++ frontend/src/i18n/i18n.tsx
@@ -3,19 +3,20 @@ import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import zh from '@/locales/zh/translation.json';
+import en from '@/locales/en/translation.json';
 
 i18n
   .use(LanguageDetector)
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: 'src/locales/{{lng}}/{{ns}}.json',
-    },
     resources: {
       'zh-CN': {
         translation: zh,
       },
+      'en': {
+        translation: en,
+      }
     },
     fallbackLng: 'en',
     interpolation: {
